80f7472491896c28b53d048f55428e8294fcb211
agathemczk <agathe.maciaszczyk@icloud.com>
2023-10-17 12:24:26 +0200

    commit

--- server.c
+++ server.c
@@ -1,57 +1,37 @@
 #include <stdio.h>
 #include <winsock2.h>
-
+#include <windows.h>
 #pragma comment(lib, "Ws2_32.lib")
 
 int main() {
     WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
+    int sockfd, newSocket;
     struct sockaddr_in serverAddr, clientAddr;
     char buffer[1024];
-    int clientAddrLen;
+    int addr_size;
 
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serverAddr, 0, sizeof(serverAddr));
+    WSAStartup(MAKEWORD(2,2), &wsaData);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serverAddr, '\0', sizeof(serverAddr));
 
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(4444);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-    listen(serverSocket, 5);
+    bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(sockfd, 5);
+    addr_size = sizeof(clientAddr);
+    newSocket = accept(sockfd, (struct sockaddr*)&clientAddr, &addr_size);
 
-    printf("Server is listening on port 4444...\n");
+    while(1) {
+        recv(newSocket, buffer, 1024, 0);
+        printf("Message received: %s\n", buffer);
 
-    while (1) {
-        clientAddrLen = sizeof(clientAddr);
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
-
-        printf("Client connected from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-
-        while (1) {
-            memset(buffer, 0, sizeof(buffer));
-            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-            if (bytesReceived <= 0) {
-                printf("Client %s:%d disconnected.\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-                closesocket(clientSocket);
-                break;
-            }
-
-            printf("Received from %s:%d: %s\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buffer);
-
-            // Envoyer le message à tous les clients connectés (y compris l'expéditeur)
-            SOCKET tempSocket = serverSocket;
-            while (tempSocket != INVALID_SOCKET) {
-                if (tempSocket != clientSocket) {
-                    send(tempSocket, buffer, bytesReceived, 0);
-                }
-                tempSocket = accept(serverSocket, NULL, NULL);
-            }
-        }
+        fgets(buffer, 1024, stdin);
+        send(newSocket, buffer, strlen(buffer), 0);
     }
 
-    closesocket(serverSocket);
+    closesocket(sockfd);
     WSACleanup();
     return 0;
+}
